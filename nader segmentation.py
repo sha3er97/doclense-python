a289a352425bb529bcd2230c0be82bfd507fcb2e
arminArlert997 <somar3296@gmail.com>
2019-12-15 18:33:19 +0200

    Create nader segmentation.py

---
+++ nader segmentation.py
@@ -0,0 +1,32 @@
+def backgroundElimination(img):
+    imgCopy = np.copy(img)
+    blockSize = 50
+    for i in range(0,img.shape[0],blockSize):
+        for j in range(0, img.shape[1], blockSize):
+            block = img[i:i + blockSize, j:j + blockSize]
+            Gmin = np.amin(block)
+            Gmax = np.amax(block)
+            Tfixed = 100
+            Tmin = 50
+            Tvar = ((Gmin - Tmin ) - min(Tfixed , Gmin - Tmin )) * 2
+            T = Tvar + Tfixed
+            intensityVariance = Gmax - Gmin
+            if(intensityVariance < T):
+                imgCopy[i:i + blockSize, j:j + blockSize] = 255
+    return imgCopy
+def binarize(img):
+    block_size = 35
+    return img > threshold_otsu(img)
+def graphicsSeparation(img):
+    structural_element = np.array([[1,1,1]
+                                  ,[1,1,1]
+                                  ,[1,1,1]])
+    imgCopy = np.copy(img)
+    
+    for i in range(10):
+        imgCopy = dilation(imgCopy, structural_element)
+        
+    for i in range(10):
+        imgCopy = reconstruction(imgCopy, img, 'erosion', structural_element)
+        
+    return imgCopy    
