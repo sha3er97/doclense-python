cf6804e179431531e77edfc3dc1bc6862464e394
[sha3er97] <37841787+sha3er97@users.noreply.github.com>
2019-12-16 17:03:24 +0200

    Update text_extraction.py

--- text_extraction.py
+++ text_extraction.py
@@ -1,6 +1,25 @@
+import matplotlib.pyplot as plt
+from skimage.color import rgb2gray,rgb2hsv,hsv2rgb
+import skimage.io as io
+from skimage.exposure import histogram
+from matplotlib.pyplot import bar
+import matplotlib as mpl
+import numpy as np
+import copy
+from scipy.signal import convolve2d
+from skimage.util import random_noise
+from skimage.exposure import rescale_intensity
+from skimage.morphology import binary_erosion, binary_dilation, binary_closing,reconstruction,dilation, erosion, opening, closing
+import PIL 
+import scipy.ndimage as ndimage
+from skimage.measure import find_contours
+from skimage.filters import threshold_minimum,median,threshold_otsu
+from skimage.transform import resize
+from skimage import img_as_bool
+
 def backgroundElimination(img):
     imgCopy = np.copy(img)
-    blockSize = 50
+    blockSize = 30
     for i in range(0,img.shape[0],blockSize):
         for j in range(0, img.shape[1], blockSize):
             block = img[i:i + blockSize, j:j + blockSize]
