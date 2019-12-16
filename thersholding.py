82fe34f577bfce5c40883c68a4665146cd87b506
[sha3er97] <37841787+sha3er97@users.noreply.github.com>
2019-12-16 17:03:16 +0200

    Update thersholding.py

--- thersholding.py
+++ thersholding.py
@@ -1,4 +1,23 @@
-def optimalThersholding(img):     
+import matplotlib.pyplot as plt
+from skimage.color import rgb2gray,rgb2hsv,hsv2rgb
+import skimage.io as io
+from skimage.exposure import histogram
+from matplotlib.pyplot import bar
+import matplotlib as mpl
+import numpy as np
+import copy
+from scipy.signal import convolve2d
+from skimage.util import random_noise
+from skimage.exposure import rescale_intensity
+from skimage.morphology import binary_erosion, binary_dilation, binary_closing,reconstruction,dilation, erosion, opening, closing
+import PIL 
+import scipy.ndimage as ndimage
+from skimage.measure import find_contours
+from skimage.filters import threshold_minimum,median,threshold_otsu
+from skimage.transform import resize
+from skimage import img_as_bool
+
+def optimalThersholding(img): #utility function
     H=np.zeros(256)
     Tint=0
     for x in img.flatten():
@@ -40,28 +59,30 @@ def optimalThersholding(img):
 
 #####################################################################################################
 
-def global_thersholding(img):
+def global_thersholding(img): #utility function
 
     return int (0.4*255) #global thershold
 
 #######################################################################################################
 
-def choose_thersholding_type(img,mode=1):
-    if(mode == 1):
+def choose_thersholding_type(img,mode): #utility function
+    if(mode == 1): #default
         return optimalThersholding(img)
     elif(mode == 2):
         return threshold_otsu(img)
+    elif(mode == 3):
+        return threshold_minimum(img)
     else:
         return global_thersholding(img)
 
 #########################################################################################################
 
-def medianFilter (img,m,n):
+def medianFilter (img,size): #utility function
     M=img.shape[0]
     N=img.shape[1]
     img2=np.ones((M,N))
-    W=int(m/2)
-    L=int(n/2)
+    W=int(size/2)
+    L=int(size/2)
     for i in range (W,M-W):
         for j in range(L,N-L):
             temp=img[i-W:i+W+1,j-L:j+L+1]
@@ -70,6 +91,41 @@ def medianFilter (img,m,n):
     return img2
 
 #########################################################################################
+def maxFilter (img,size): #utility function
+    M=img.shape[0]
+    N=img.shape[1]
+    img2=np.ones((M,N))
+    W=int(size/2)
+    L=int(size/2)
+    for i in range (W,M-W):
+        for j in range(L,N-L):
+            temp=img[i-W:i+W+1,j-L:j+L+1]
+            maxim=np.median(temp.flatten())
+            img2[i,j]=int(maxim)
+    return img2
+###############################################################################################
+def minFilter (img,size): #utility function
+    M=img.shape[0]
+    N=img.shape[1]
+    img2=np.ones((M,N))
+    W=int(size/2)
+    L=int(size/2)
+    for i in range (W,M-W):
+        for j in range(L,N-L):
+            temp=img[i-W:i+W+1,j-L:j+L+1]
+            mini=np.median(temp.flatten())
+            img2[i,j]=int(mini)
+    return img2
+################################################################################################
+def binarize(img,mode=1): #interface function
+    return 1*(img > choose_thersholding_type(img,mode))
 
-def binarize(img,mode=1):
-    return img > choose_thersholding_type(img,mode)
+################################################################################################
+
+def apply_filter(img,size,mode=1): #interface function
+    if(mode == 1): #default
+        return maxFilter(img,size) #brighter image
+    elif(mode == 2):
+        return minFilter(img,size) #darker image
+    else:
+        return medianFilter(img,size)
